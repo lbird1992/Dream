d274d7bb148c3fb3d092e6af5bd37921bdf2a3d6
Bo Li <18642304836@163.com>
2013-10-26 13:26:45 +0800

    Version 0.3.1.1

--- game_logic.cc
+++ game_logic.cc
@@ -1,11 +1,14 @@
-#include "game_logic.h"
+﻿#include "game_logic.h"
 #include "game_manage.h"
 
 const int MAX = 32;
+
+GameLogic* g_game_logic;
+
 void GameLogic::LoadMapIni() {
   FILE* fp = fopen( "data\\map.ini","r");
   for( int i=1; ; ++i) {
-    if( fscanf(fp, "%d %s", &map_data_[i].map_image, map_name) == EOF)
+    if( fscanf(fp, "%d %s", &map_data_[i].map_image, map_data_[i].map_name) == EOF)
       break;
   }
   fclose(fp);
@@ -15,7 +18,6 @@ void GameLogic::Walk() {
   float x_length = (map_coordinate_to_go_ - map_coordinate_).GetX();
   float y_length = (map_coordinate_to_go_ - map_coordinate_).GetY();
   if( x_length * x_length + y_length * y_length < kWalkSpeed * kWalkSpeed) {
-    //ƫ��������һ֡�ƶ�
     map_coordinate_ = map_coordinate_to_go_;
     g_game_manage->GetMap()->GetPlayerGUI()->EndAnimation();
     return;
@@ -44,7 +46,6 @@ void GameLogic::Walk() {
   else if( g_game_manage->GetMap()->GetCell( 
                                     map_coordinate_ + Coordinate(static_cast<float>(x_speed),
                                                                 static_cast<float>(y_speed))) == 2) {
-    //TODO:找到传送点
     int x = static_cast<int>(map_coordinate_.GetX() + x_speed);
     int y = static_cast<int>(map_coordinate_.GetY() + y_speed);
     std::list<TransformData> transform_data = g_game_manage->GetMap()->GetTransformData();
@@ -89,18 +90,14 @@ void GameLogic::Walk() {
 
 
 int Player::NewPlayer(int ID,char* name,int image){
-  memset(this,0,sizeof(Player));
-  char name[256];
-	
-	
 	Player hero;
-	
+  memset( &hero, 0, sizeof(hero));
 	hero.ID_ = ID;
 	strcpy( hero.name, name);
 	hero.image_ = image;
 
 	if( image_/4 == 0)
-	{//����
+	{
 		hero.tizhi_ = hero.moli_ = hero.liliang_ = hero.naili_ = hero.minjie_ = 10;
 		hero.hp_ = hero.hp2_ = hero.hp3_ = 150;
 		hero.mp_ = hero.mp2_ = 110;
@@ -112,28 +109,28 @@ int Player::NewPlayer(int ID,char* name,int image){
 		hero.lingli_ = 16;
 	}
 	else if( image_/4 == 1)
-	{//����
+	{
 		hero.tizhi_ = 12, hero.moli_ = 5, hero.liliang_ = 11, hero.naili_ = 12, hero.minjie_ = 10;
 		hero.hp_ = hero.hp2_ = hero.hp3_ = 154;
 		hero.mp_ = hero.mp2_ = 97.5;
-		hero.shanghai_ = 46.6;
-		hero.mingzhong_ = 48.7;
-		hero.fangyu_ = 19.2;
+		hero.shanghai_ = 46.6f;
+		hero.mingzhong_ = 48.7f;
+		hero.fangyu_ = 19.2f;
 		hero.duobi_ = 10;
-		hero.sudu_ = 10.5;
-		hero.lingli_ = 13.9;
+		hero.sudu_ = 10.5f;
+		hero.lingli_ = 13.9f;
 	}
 	else
-	{//ħ��
+	{
 		hero.tizhi_ = 12, hero.moli_ = 11, hero.liliang_ = 11, hero.naili_ = 8, hero.minjie_ = 8;
 		hero.hp_ = hero.hp2_ = hero.hp3_ = 172;
 		hero.mp_ = hero.mp2_ = 107.5;
-		hero.shanghai_ = 42.8;
-		hero.mingzhong_ = 52.3;
-		hero.fangyu_ = 10.4;
+		hero.shanghai_ = 42.8f;
+		hero.mingzhong_ = 52.3f;
+		hero.fangyu_ = 10.4f;
 		hero.duobi_ = 8;
-		hero.sudu_ = 8.7;
-		hero.lingli_ = 17.3;
+		hero.sudu_ = 8.7f;
+		hero.lingli_ = 17.3f;
 	}
 	hero.mapID_ = 1;
 	hero.mapX_ = hero.mapAimX_ = 30;
@@ -141,6 +138,8 @@ int Player::NewPlayer(int ID,char* name,int image){
 	hero.tili2_ = hero.huoli2_ = 100;
 	hero.xp_ = 0;
 	
+  *this = hero;
+
 	return 0;
 }
 
@@ -148,41 +147,40 @@ void Player::ChangePoint(int playerID, int tizhi, int moli, int liliang, int nai
  /* mpHeroLock.Lock();
 	Hero hero = mpHero[ playerID];
 	mpHeroLock.Unlock();*/
-  Player hero;
+  Player hero = *this;
 	if( tizhi + moli + liliang + naili + minjie > hero.qianli_)
 		return;
-	if( hero.image_/4 == 0)//����
+	if( hero.image_/4 == 0)
 	{
 		hero.hp3_ += tizhi*5;
 		hero.mp2_ += moli*3;
-		hero.shanghai_ += liliang*0.67;
-		hero.mingzhong_ += liliang*2.01;
-		hero.fangyu_ += naili*1.5;
+		hero.shanghai_ += liliang*0.67f;
+		hero.mingzhong_ += liliang*2.01f;
+		hero.fangyu_ += naili*1.5f;
 	}
-	else if( hero.image_/4 == 1)//����
+	else if( hero.image_/4 == 1)
 	{
-		hero.hp3_ += tizhi*4.5;
-		hero.mp2_ += moli*3.5;
-		hero.shanghai_ += liliang*0.57;
-		hero.mingzhong_ += liliang*1.71;
-		hero.fangyu_ += naili*1.6;
+		hero.hp3_ += tizhi*4.5f;
+		hero.mp2_ += moli*3.5f;
+		hero.shanghai_ += liliang*0.57f;
+		hero.mingzhong_ += liliang*1.71f;
+		hero.fangyu_ += naili*1.6f;
 	}
-	else//ħ��
+	else
 	{
 		hero.hp3_ += tizhi*6;
-		hero.mp2_ += moli * 2.5;
-		hero.shanghai_ += liliang*0.77;
-		hero.mingzhong_ += liliang*2.31;
-		hero.fangyu_ += naili*1.4;
+		hero.mp2_ += moli * 2.5f;
+		hero.shanghai_ += liliang*0.77f;
+		hero.mingzhong_ += liliang*2.31f;
+		hero.fangyu_ += naili*1.4f;
 	}
-	hero.lingli_ += tizhi*0.3 + moli * 0.7 + naili*0.2 + liliang*0.4;
-	hero.sudu_ += tizhi*0.1 + naili*0.1 + liliang*0.1 + minjie * 0.7;
+	hero.lingli_ += tizhi*0.3f + moli * 0.7f + naili*0.2f + liliang*0.4f;
+	hero.sudu_ += tizhi*0.1f + naili*0.1f + liliang*0.1f + minjie * 0.7f;
 	hero.duobi_ += minjie;
 	if( tizhi != 0)
 		hero.hp_ = hero.hp2_ = hero.hp3_;
 	if( moli != 0)
 		hero.mp_ = hero.mp2_;
-	//ǿ��ڤ��������Ӱ��δ����
 
 	hero.qianli_ -= tizhi + moli + liliang + naili + minjie;
 	hero.tizhi_ += tizhi;
@@ -190,98 +188,91 @@ void Player::ChangePoint(int playerID, int tizhi, int moli, int liliang, int nai
 	hero.liliang_ += liliang;
 	hero.naili_ += naili;
 	hero.minjie_ += minjie;
-	//mpHeroLock.Lock();
-	//mpHero[ playerID] = hero;
-	//mpHeroLock.Unlock();
+  *this = hero;
 }
 
-void Player::LoadPlayer(FILE* fc) const{
-  int i = 0,j,k;
+void Player::LoadPlayer(FILE* fc) {
   Player temp;
-  for(i=0;i<MAX;i++){
-     fread( &temp.ID_, sizeof(ID_), 1, fc);
-     fread( &temp.image_, sizeof(temp.image_), 1, fc);
-     fread( &temp.weaponImage_, sizeof(temp.weaponImage_), 1, fc);
-     fread(temp.name,sizeof(char), 20, fc);
-     fread( &temp.renqi_, sizeof(temp.renqi_), 1, fc);
-     fread( &temp.menpai_, sizeof(temp.menpai_), 1, fc);
-     fread( &temp.menpaigongxian_, sizeof(temp.menpaigongxian_), 1, fc);
-     fread( &temp.money_, sizeof(temp.money_), 1, fc);
-     fread( &temp.bank_, sizeof(temp.bank_), 1, fc);
-     fread( &temp.hp_, sizeof(temp.hp_), 1, fc);
-     fread( &temp.hp2_, sizeof(temp.hp2_), 1, fc);
-     fread( &temp.hp3_, sizeof(temp.hp3_), 1,fc);
-     fread( &temp.mp_, sizeof(temp.mp_), 1, fc);
-     fread( &temp.mp2_, sizeof(temp.mp2_), 1, fc);
-     fread( &temp.sp_, sizeof(temp.sp_), 1,fc);
-     fread( &temp.huoli_, sizeof(temp.huoli_), 1, fc);
-     fread( &temp.huoli2_, sizeof(temp.huoli2_), 1, fc);
-     fread( &temp.tili_, sizeof(temp.tili_), 1, fc);
-     fread( &temp.tili2_, sizeof(temp.tili2_), 1, fc);
+  if( fc != NULL) {
+    fread( &temp.ID_, sizeof(ID_), 1, fc);
+    fread( &temp.image_, sizeof(temp.image_), 1, fc);
+    fread( &temp.weaponImage_, sizeof(temp.weaponImage_), 1, fc);
+    fread(temp.name,sizeof(char), 20, fc);
+    fread( &temp.renqi_, sizeof(temp.renqi_), 1, fc);
+    fread( &temp.menpai_, sizeof(temp.menpai_), 1, fc);
+    fread( &temp.menpaigongxian_, sizeof(temp.menpaigongxian_), 1, fc);
+    fread( &temp.money_, sizeof(temp.money_), 1, fc);
+    fread( &temp.bank_, sizeof(temp.bank_), 1, fc);
+    fread( &temp.hp_, sizeof(temp.hp_), 1, fc);
+    fread( &temp.hp2_, sizeof(temp.hp2_), 1, fc);
+    fread( &temp.hp3_, sizeof(temp.hp3_), 1,fc);
+    fread( &temp.mp_, sizeof(temp.mp_), 1, fc);
+    fread( &temp.mp2_, sizeof(temp.mp2_), 1, fc);
+    fread( &temp.sp_, sizeof(temp.sp_), 1,fc);
+    fread( &temp.huoli_, sizeof(temp.huoli_), 1, fc);
+    fread( &temp.huoli2_, sizeof(temp.huoli2_), 1, fc);
+    fread( &temp.tili_, sizeof(temp.tili_), 1, fc);
+    fread( &temp.tili2_, sizeof(temp.tili2_), 1, fc);
 
-     fread( &temp.mingzhong_, sizeof(temp.mingzhong_), 1, fc);
-     fread( &temp.shanghai_, sizeof(temp.shanghai_), 1, fc);
-     fread( &temp.fangyu_, sizeof(temp.fangyu_), 1, fc);
+    fread( &temp.mingzhong_, sizeof(temp.mingzhong_), 1, fc);
+    fread( &temp.shanghai_, sizeof(temp.shanghai_), 1, fc);
+    fread( &temp.fangyu_, sizeof(temp.fangyu_), 1, fc);
 
-     fread( &temp.sudu_, sizeof(temp.sudu_), 1, fc);
-     fread( &temp.duobi_, sizeof(temp.duobi_), 1, fc);
-     fread( &temp.lingli_, sizeof(temp.lingli_), 1, fc);
-     fread( &temp.tizhi_, sizeof(temp.tizhi_), 1, fc);
-     fread( &temp.moli_, sizeof(temp.moli_), 1, fc);
-     fread( &temp.liliang_, sizeof(temp.liliang_), 1, fc);
-     fread( &temp.naili_, sizeof(temp.naili_), 1, fc);
-     fread( &temp.minjie_, sizeof(temp.minjie_), 1, fc);
-     fread( &temp.qianli_, sizeof(temp.qianli_), 1, fc);
-     fread( &temp.xp_, sizeof(temp.xp_), 1, fc);
-	}
+    fread( &temp.sudu_, sizeof(temp.sudu_), 1, fc);
+    fread( &temp.duobi_, sizeof(temp.duobi_), 1, fc);
+    fread( &temp.lingli_, sizeof(temp.lingli_), 1, fc);
+    fread( &temp.tizhi_, sizeof(temp.tizhi_), 1, fc);
+    fread( &temp.moli_, sizeof(temp.moli_), 1, fc);
+    fread( &temp.liliang_, sizeof(temp.liliang_), 1, fc);
+    fread( &temp.naili_, sizeof(temp.naili_), 1, fc);
+    fread( &temp.minjie_, sizeof(temp.minjie_), 1, fc);
+    fread( &temp.qianli_, sizeof(temp.qianli_), 1, fc);
+    fread( &temp.xp_, sizeof(temp.xp_), 1, fc);
+  }
   fclose(fc);
+  *this = temp;
 }
 
 
 void Player::Save(FILE* fa) const{
-  int i;
-  Player temp;
-  
+  Player temp = *this;
   if(fa != NULL){
-    for(i = 0;i<MAX;i++){
-     fwrite( &temp.ID_, sizeof(ID_), 1, fa);
-     fwrite( &temp.image_, sizeof(temp.image_), 1, fa);
-     fwrite( &temp.weaponImage_, sizeof(temp.weaponImage_), 1, fa);
-     fwrite(temp.name,sizeof(char), 20, fa);
-     fwrite( &temp.renqi_, sizeof(temp.renqi_), 1, fa);
-     fwrite( &temp.menpai_, sizeof(temp.menpai_), 1, fa);
-     fwrite( &temp.menpaigongxian_, sizeof(temp.menpaigongxian_), 1, fa);
-     fwrite( &temp.money_, sizeof(temp.money_), 1, fa);
-     fwrite( &temp.bank_, sizeof(temp.bank_), 1, fa);
-     fwrite( &temp.hp_, sizeof(temp.hp_), 1, fa);
-     fwrite( &temp.hp2_, sizeof(temp.hp2_), 1, fa);
-     fwrite( &temp.hp3_, sizeof(temp.hp3_), 1,fa);
-     fwrite( &temp.mp_, sizeof(temp.mp_), 1, fa);
-     fwrite( &temp.mp2_, sizeof(temp.mp2_), 1, fa);
-     fwrite( &temp.sp_, sizeof(temp.sp_), 1,fa);
-     fwrite( &temp.huoli_, sizeof(temp.huoli_), 1, fa);
-     fwrite( &temp.huoli2_, sizeof(temp.huoli2_), 1, fa);
-     fwrite( &temp.tili_, sizeof(temp.tili_), 1, fa);
-     fwrite( &temp.tili2_, sizeof(temp.tili2_), 1, fa);
+    fwrite( &temp.ID_, sizeof(ID_), 1, fa);
+    fwrite( &temp.image_, sizeof(temp.image_), 1, fa);
+    fwrite( &temp.weaponImage_, sizeof(temp.weaponImage_), 1, fa);
+    fwrite(temp.name,sizeof(char), 20, fa);
+    fwrite( &temp.renqi_, sizeof(temp.renqi_), 1, fa);
+    fwrite( &temp.menpai_, sizeof(temp.menpai_), 1, fa);
+    fwrite( &temp.menpaigongxian_, sizeof(temp.menpaigongxian_), 1, fa);
+    fwrite( &temp.money_, sizeof(temp.money_), 1, fa);
+    fwrite( &temp.bank_, sizeof(temp.bank_), 1, fa);
+    fwrite( &temp.hp_, sizeof(temp.hp_), 1, fa);
+    fwrite( &temp.hp2_, sizeof(temp.hp2_), 1, fa);
+    fwrite( &temp.hp3_, sizeof(temp.hp3_), 1,fa);
+    fwrite( &temp.mp_, sizeof(temp.mp_), 1, fa);
+    fwrite( &temp.mp2_, sizeof(temp.mp2_), 1, fa);
+    fwrite( &temp.sp_, sizeof(temp.sp_), 1,fa);
+    fwrite( &temp.huoli_, sizeof(temp.huoli_), 1, fa);
+    fwrite( &temp.huoli2_, sizeof(temp.huoli2_), 1, fa);
+    fwrite( &temp.tili_, sizeof(temp.tili_), 1, fa);
+    fwrite( &temp.tili2_, sizeof(temp.tili2_), 1, fa);
 
-     fwrite( &temp.mingzhong_, sizeof(temp.mingzhong_), 1, fa);
-     fwrite( &temp.shanghai_, sizeof(temp.shanghai_), 1, fa);
-     fwrite( &temp.fangyu_, sizeof(temp.fangyu_), 1, fa);
+    fwrite( &temp.mingzhong_, sizeof(temp.mingzhong_), 1, fa);
+    fwrite( &temp.shanghai_, sizeof(temp.shanghai_), 1, fa);
+    fwrite( &temp.fangyu_, sizeof(temp.fangyu_), 1, fa);
 
-     fwrite( &temp.sudu_, sizeof(temp.sudu_), 1, fa);
-     fwrite( &temp.duobi_, sizeof(temp.duobi_), 1, fa);
-     fwrite( &temp.lingli_, sizeof(temp.lingli_), 1, fa);
-     fwrite( &temp.tizhi_, sizeof(temp.tizhi_), 1, fa);
-     fwrite( &temp.moli_, sizeof(temp.moli_), 1, fa);
-     fwrite( &temp.liliang_, sizeof(temp.liliang_), 1, fa);
-     fwrite( &temp.naili_, sizeof(temp.naili_), 1, fa);
-     fwrite( &temp.minjie_, sizeof(temp.minjie_), 1, fa);
-     fwrite( &temp.qianli_, sizeof(temp.qianli_), 1, fa);
-     fwrite( &temp.xp_, sizeof(temp.xp_), 1, fa);
-    }
+    fwrite( &temp.sudu_, sizeof(temp.sudu_), 1, fa);
+    fwrite( &temp.duobi_, sizeof(temp.duobi_), 1, fa);
+    fwrite( &temp.lingli_, sizeof(temp.lingli_), 1, fa);
+    fwrite( &temp.tizhi_, sizeof(temp.tizhi_), 1, fa);
+    fwrite( &temp.moli_, sizeof(temp.moli_), 1, fa);
+    fwrite( &temp.liliang_, sizeof(temp.liliang_), 1, fa);
+    fwrite( &temp.naili_, sizeof(temp.naili_), 1, fa);
+    fwrite( &temp.minjie_, sizeof(temp.minjie_), 1, fa);
+    fwrite( &temp.qianli_, sizeof(temp.qianli_), 1, fa);
+    fwrite( &temp.xp_, sizeof(temp.xp_), 1, fa);
   }
   fclose(fa);
-  
 }
 
 
--- game_logic.h
+++ game_logic.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef _DREAM_GAMELOGIC_
 #define _DREAM_GAMELOGIC_
 
@@ -11,6 +10,7 @@ const double kWalkSpeed = 0.27;//�����ٶ�
 
 struct MapData {
   int map_image;
+  char map_name[256];
 };
 
 class GameLogic {
@@ -64,17 +64,6 @@ private:
 };
 
 extern GameLogic* g_game_logic;
-
-=======
-﻿#ifndef _DREAM_GAMELOGIC_
-#define _DREAM_GAMELOGIC_
-
-#include <stdio.h>
-#include <stdint.h>
-#include "hge_include.h"
-
-const int kMaxMapCount = 100;//游戏中地图单体的最大数量
-const double kWalkSpeed = 0.27;//行走速度
 
 enum HeroImage{
   HI_NONE = 0,
@@ -190,13 +179,12 @@ enum HeroImage{
   HI_BIAOTOU = 115
 };
 
-
 class Hero{
 public: 
   int ID_;
 	int image_;
 	int weaponImage_;
-	char name[20]_;
+	char name_[20];
 	int mapID_;
 	int mapX_;
 	int mapY_;
@@ -210,8 +198,6 @@ public:
 	float sudu_;
 	float duobi_;
 	float lingli_;
-
-
 };
 
 class Player:public Hero{
@@ -223,7 +209,6 @@ public:
 	int mapID_;
 	int mapX_;
 	int mapY_;
-	
 	int renqi_;
 	int menpai_;
 	int menpaigongxian_;
@@ -274,65 +259,11 @@ public:
 public:
   int NewPlayer(int ID,char* name,int image);
   void ChangePoint(int playerID, int tizhi, int moli, int liliang, int naili, int minjie);
-  void Save();
-  void LoadPlayer();
+  void Save( FILE* fa) const;
+  void LoadPlayer( FILE* fc);
 };
-
 
 /*class Enemy : public Hero{
 
 }*/
-
-
-struct MapData {
-  int map_image;
-  int map_x_max;
-  int map_y_max;
-};
-
-class GameLogic {
-public:
-  inline Coordinate GetPlayerCoordinate() const {
-    return map_coordinate_;
-  }
-  inline void SetPlayerCoordinate( const Coordinate coordinate) {
-    map_coordinate_ = coordinate;
-  }
-  inline int GetMapID() const {
-    return map_id_;
-  }
-  inline void SetMapID( const int map_id) {
-    map_id_ = map_id;
-  }
-  inline int GetPlayerDirection() const {
-    return player_direction_;
-  }
-  inline void SetPlayerDirection( const int direction) {
-    player_direction_ = direction;
-  }
-  void LoadMapIni();
-  inline MapData GetMapData( const int map_id) const {
-    return map_data_[map_id];
-  }
-
-  inline Coordinate GetMapAim() const {
-    return map_coordinate_to_go_;
-  }
-  inline void SetMapAim( const Coordinate aim) {
-    map_coordinate_to_go_ = aim;
-  }
-
-  void Walk();//人物行走，此处只更改人物坐标，画面中心坐标在GUIMAP中实现
-
-private:
-  int map_id_;
-  Coordinate map_coordinate_;
-  Coordinate map_coordinate_to_go_;//目标点坐标
-  int player_direction_;
-  MapData map_data_[kMaxMapCount];
-};
-
-extern GameLogic* g_game_logic;
-
->>>>>>> 9d0fb050f4cf5cb2ef278700b1d7d1bfbd87eba5
-#endif //_DREAM_GAMELOGIC
+#endif
